7e535cefc21f0441ac67b35239f962b6d6d64212
waterflow80 <thessalonikaathena@outlook.com>
2022-02-06 23:32:55 +0100

    source code

---
+++ code/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"menu.h"
+
+
+int main(){
+  printf("hello\n");
+  /*Menu Section*/
+  mainMenu();
+  printf("bye\n");
+  return 0;
+}
---
+++ code/menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H
+#define MENU_H
+
+
+
+void mainMenu(void);
+
+#endif
